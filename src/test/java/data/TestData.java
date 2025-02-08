57da7a509b7559e1579d6fa2020e52a798e7752a
Alexanderrr123193 <reshetnikovalexander1992@gmail.com>
2025-02-08 18:19:48 +0300

    add owner fix7

--- src/test/java/data/TestData.java
+++ src/test/java/data/TestData.java
@@ -1,9 +1,8 @@
 package data;
 
 public class TestData {
-    protected static final String bookStoreLogin = System.getProperty("bookStoreLogin");
-    protected static final String bookStorePassword = System.getProperty("bookStorePassword");
+    protected static final String bookStoreLogin = System.getProperty("bookStoreLogin", "defaultLogin");
+    protected static final String bookStorePassword = System.getProperty("bookStorePassword", "defaultPassword");
     public static final String testBookIsbn = "9781449365035";
-
     public static final String bookName = "Speaking JavaScript";
 }
